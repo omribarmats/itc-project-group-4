d695fec1b820bd463bdc0083c8b7e859ef4e3d26
omribarmats <omri.bar@gmail.com>
2023-03-22 16:21:05 +0200

    22-03-2023

--- Backend/index.js
+++ Backend/index.js
@@ -30,10 +30,48 @@ app.post("/day-planner", async (req, res) => {
       presence_penalty: 0,
       // stop: ["\n"],
     });
+    const responseText = response.data.choices[0].text.replace(/\n/g, "");
+    return res.status(200).json({
+      success: "true",
+      data: responseText,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: error.response
+        ? error.response.data
+        : "There was an issue on the server",
+    });
+  }
+});
+
+app.post("/day-planner-chat", async (req, res) => {
+  try {
+    const { amount } = req.body;
+    const { activity } = req.body;
+    const { city } = req.body;
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `return a JS array of objects of ${amount} coordinates and names of ${activity} in ${city}. Drop any from the answer.
+      For example: [{name: "Cafe de Flore", coordinates: [48.863096, 2.332536]}, [{name: "Les Deux Magots", coordinates: [48.859809, 2.334535]}]
+      `,
+        },
+      ],
+      temperature: 0.8,
+      max_tokens: 2506,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      // stop: ["\n"],
+    });
     console.log(response);
     return res.status(200).json({
       success: "true",
-      data: response.data.choices[0].text,
+      data: response.data.choices[0].message,
     });
   } catch (error) {
     return res.status(400).json({
