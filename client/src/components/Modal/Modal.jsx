a664560850a1ecf0e76edf02322d32459af475e0
omribarmats <omri.bar@gmail.com>
2023-03-22 16:26:38 +0200

    Test
    Testy

---
+++ client/src/components/Modal/Modal.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UiDialog from "../../ui/uiKit/componentsUi/UiDialog";
+
+export default function Modal() {
+  const handleOnClose = () => {
+    console.log("close modal");
+  };
+
+  //Testy
+
+  return (
+    <UiDialog open={false} onClose={handleOnClose}>
+      <div>Modal</div>
+    </UiDialog>
+  );
+}
