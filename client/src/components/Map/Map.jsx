7ffe8610916feb33f85d493c5e1d32756467cf54
jgelbwaser <99264301+jgelbwaser@users.noreply.github.com>
2023-03-23 08:58:33 +0200

    wip connect map to api

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -9,6 +9,8 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import useApi from '../../api/useApi';
 import LocationInfo from '../Modal/LocationInfo';
+import { useSelector } from 'react-redux'
+import { setDestination } from '../../state/reducers/mapSlice';
 
 const top10Locations = [
   {
@@ -68,7 +70,8 @@ const top10Locations = [
 export default function LeafletMap() {
 
   const { getLocationsWithFilters} = useApi()
-
+  const {foundLocations} = useSelector(state = state.map)
+  const mapCenter = [foundLocations[0]?.latitude , foundLocations[0]?.longitude]
 
   useEffect(() => {
     delete L.Icon.Default.prototype._getIconUrl;
@@ -79,6 +82,13 @@ export default function LeafletMap() {
     });
       getLocationsWithFilters()
   }, []);
+
+  useEffect(()=>{ dispatch(
+    setDestination({
+      name: , 
+      coords:  [foundLocations[0]?.latitude , foundLocations[0]?.longitude]
+    }))
+  },[foundLocations])
 
   const handleMarkerClick = (locationName) => {
     console.log(`The location ${locationName} has been saved`)
@@ -97,6 +107,8 @@ export default function LeafletMap() {
   
   const mapStyle = { height: '100vh' }; // set the desired height in pixels
   
+//center={[48.8566, 2.3522]} 
+
   return (
 <MapContainer center={[48.8566, 2.3522]} zoom={13} scrollWheelZoom={false} style={mapStyle}>
   <TileLayer
