8bb4532b3d376481f4be33878cef219f0054262d
omribarmats <omri.bar@gmail.com>
2023-03-23 16:05:22 +0200

    Minor changes
    Minor changes

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,74 +1,70 @@
-import React from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // import the CSS for Leaflet
-import L from 'leaflet';
-import { useEffect } from 'react';
+import React from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // import the CSS for Leaflet
+import L from "leaflet";
+import { useEffect } from "react";
 
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import useApi from '../../api/useApi';
-import LocationInfo from '../Modal/LocationInfo';
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import useApi from "../../api/useApi";
+import LocationInfo from "../Modal/LocationInfo";
 
 const top10Locations = [
   {
-    name: 'Eiffel Tower',
+    name: "Eiffel Tower",
     latlng: [48.8584, 2.2945],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Louvre Museum',
+    name: "Louvre Museum",
     latlng: [48.8606, 2.3376],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Notre-Dame Cathedral',
+    name: "Notre-Dame Cathedral",
     latlng: [48.8529, 2.3499],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Montmartre',
+    name: "Montmartre",
     latlng: [48.8867, 2.3431],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Arc de Triomphe',
+    name: "Arc de Triomphe",
     latlng: [48.8738, 2.295],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Champs-Élysées',
+    name: "Champs-Élysées",
     latlng: [48.8698, 2.3078],
-    ranking: 4.5
+    ranking: 4.5,
   },
   {
-    name: 'Palace of Versailles',
+    name: "Palace of Versailles",
     latlng: [48.8048, 2.1204],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Musée d\'Orsay',
+    name: "Musée d'Orsay",
     latlng: [48.8599, 2.3266],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Sainte-Chapelle',
+    name: "Sainte-Chapelle",
     latlng: [48.8554, 2.3447],
-    ranking: 4.6
+    ranking: 4.6,
   },
   {
-    name: 'Centre Pompidou',
+    name: "Centre Pompidou",
     latlng: [48.8606, 2.3522],
-    ranking: 4.5
-  }
+    ranking: 4.5,
+  },
 ];
 
-
-
 export default function LeafletMap() {
-
-  const { getLocationsWithFilters} = useApi()
-
+  const { getLocationsWithFilters } = useApi();
 
   useEffect(() => {
     delete L.Icon.Default.prototype._getIconUrl;
@@ -77,16 +73,16 @@ export default function LeafletMap() {
       iconUrl: markerIcon,
       shadowUrl: markerShadow,
     });
-      getLocationsWithFilters()
+    getLocationsWithFilters();
   }, []);
 
   const handleMarkerClick = (locationName) => {
-    console.log(`The location ${locationName} has been saved`)
-  }
+    console.log(`The location ${locationName} has been saved`);
+  };
 
-  const CustomPopup = ({googleRank, locationName}) => (
-    <Popup>
-      <LocationInfo/>
+  const CustomPopup = ({ googleRank, locationName }) => (
+    <Popup minWidth="100px">
+      <LocationInfo />
       {/* <div>
         <h3>Visit  {locationName}</h3>
         <p>Google ranking: {googleRank}</p>
@@ -94,25 +90,33 @@ export default function LeafletMap() {
       </div> */}
     </Popup>
   );
-  
-  const mapStyle = { height: '100vh' }; // set the desired height in pixels
-  
-  return (
-<MapContainer center={[48.8566, 2.3522]} zoom={13} scrollWheelZoom={false} style={mapStyle}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
 
-  {top10Locations.map(location => (
-        <Marker key={location.name} position={location.latlng} onClick={handleMarkerClick}>
-           <CustomPopup googleRank={location.ranking} locationName={location.name}/>
+  const mapStyle = { height: "100vh" }; // set the desired height in pixels
+
+  return (
+    <MapContainer
+      center={[48.8566, 2.3522]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={mapStyle}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {top10Locations.map((location) => (
+        <Marker
+          key={location.name}
+          position={location.latlng}
+          onClick={handleMarkerClick}
+        >
+          <CustomPopup
+            googleRank={location.ranking}
+            locationName={location.name}
+          />
         </Marker>
       ))}
-</MapContainer>
-
-
-   )
+    </MapContainer>
+  );
 }
-
-
--- client/src/components/Modal/LocationInfo.jsx
+++ client/src/components/Modal/LocationInfo.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./LocationInfo.css";
 
 export default function LocationInfo() {
+  const handleSaveLocation = () => {
+    console.log("handleSaveLocation");
+  };
+
   return (
-    <div>Location</div>
-  )
+    <div className="location">
+      <h2>Type</h2>
+      <h2>Name</h2>
+      <h3>Description</h3>
+      <p>Address Address Address Address Address</p>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleSaveLocation();
+        }}
+      >
+        Save
+      </button>
+    </div>
+  );
 }
