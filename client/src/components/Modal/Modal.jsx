61f8419768dc6a4bacb889a7c6a703e9a036377a
omribarmats <omri.bar@gmail.com>
2023-03-25 14:42:12 +0300

    Edited search form
    1. Removed autocomplete functionality
    2. City and activities are console logged on Submit button click

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import UiDialog from '../../ui/uiKit/componentsUi/UiDialog'
-import SearchForm from './SearchForm'
-import {useDispatch, useSelector} from 'react-redux'
-import { closeModal } from '../../state/reducers/appSlice'
+import React from "react";
+import UiDialog from "../../ui/uiKit/componentsUi/UiDialog";
+import SearchForm from "./SearchForm";
+import { useDispatch, useSelector } from "react-redux";
+import { closeModal } from "../../state/reducers/appSlice";
 
 export default function Modal() {
+  const { isOpen } = useSelector((state) => state.app.modal);
+  const dispatch = useDispatch();
 
-    const {isOpen} = useSelector(state => state.app.modal)
-    const dispatch = useDispatch()
+  const handleOnClose = () => {
+    console.log("close modal");
+    dispatch(closeModal());
+  };
 
-    const handleOnClose = () => {
-        console.log('close modal')
-        dispatch(closeModal())    
-    }
-
-    return (
-        <UiDialog open={isOpen} onClose={handleOnClose}>
-            <SearchForm />
-        </UiDialog>
-    )
+  return (
+    <UiDialog open={isOpen} onClose={handleOnClose}>
+      <SearchForm />
+    </UiDialog>
+  );
 }
--- client/src/components/Modal/SearchForm.jsx
+++ client/src/components/Modal/SearchForm.jsx
@@ -1,32 +1,101 @@
 import { Box, Button } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import { createFilterOptions } from "@mui/material/Autocomplete";
-import { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { clearAppError } from "../../state/reducers/appSlice";
+import { useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+import { useState } from "react";
 
+const activitiesArray = [
+  "Cafes",
+  "Parks",
+  "Restaurants",
+  "Museums",
+  "Start-ups",
+  "Bars",
+  "Malls",
+  "Markets",
+  "Hotels",
+  "Hostels",
+];
+
+const citiesArray = [
+  "Tokyo",
+  "New Delhi",
+  "Beijing",
+  "Moscow",
+  "Seoul",
+  "Berlin",
+  "Jakarta",
+  "London",
+  "Brasília",
+  "Canberra",
+  "Ottawa",
+  "Washington D.C.",
+  "Madrid",
+  "Paris",
+  "Rome",
+  "Amsterdam",
+  "Buenos Aires",
+  "Cairo",
+  "Dublin",
+  "Oslo",
+  "Lisbon",
+  "Stockholm",
+  "Athens",
+  "Vienna",
+  "Brussels",
+  "Helsinki",
+  "Prague",
+  "Warsaw",
+  "Budapest",
+  "Copenhagen",
+  "Reykjavik",
+  "Nicosia",
+  "Tallinn",
+  "Riga",
+  "Vilnius",
+  "Ljubljana",
+  "Zagreb",
+  "Sofia",
+  "Bucharest",
+  "Belgrade",
+  "Skopje",
+  "Podgorica",
+  "Tirana",
+  "Chisinau",
+  "Minsk",
+  "Kiev",
+  "Tbilisi",
+  "Yerevan",
+  "Baku",
+];
 
 export default function SearchForm() {
+  const { loading, error } = useSelector((state) => state.app);
+  const dispatch = useDispatch();
+  const [activities, setActivities] = useState([]);
+  const [city, setCity] = useState("");
 
-  const {loading, error} = useSelector(state => state.app)
-  const dispatch = useDispatch()
- 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(clearAppError())
+    e.preventDefault();
+    dispatch(clearAppError());
     const formData = new FormData(e.currentTarget);
-    const type = formData.get('type');
-    const name = formData.get('name')
-    console.log('type', type)
-    console.log('name', name)
-    console.log('formData.entries' ,formData.entries())
+    const type = formData.get("type");
+    const name = formData.get("name");
+    console.log("type", type);
+    console.log("name", name);
+    console.log("formData.entries", formData.entries());
+    console.log("city:", city, "activities:", activities);
 
     for (const pair of formData.entries()) {
       console.log(`${pair[0]}, ${pair[1]}`);
     }
-    
+
     // for (let i = 0; i < form.elements.length; i++) {
     //   const element = form.elements[i];
     //   if (element.name) {
@@ -34,193 +103,90 @@ export default function SearchForm() {
     //   }
     // }
 
+    console.log("form submited");
+  };
 
+  const handleActivityChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setActivities(typeof value === "string" ? value.split(",") : value);
+  };
 
-    console.log('form submited')
-  
-  }
-
-
-  const activities = [
-    { title: "Cafes" },
-    { title: "Parks" },
-    { title: "Restaurants" },
-    { title: "Museums" },
-    { title: "Start-ups" },
-    { title: "Bars" },
-    { title: "Malls" },
-    { title: "Markets" },
-    { title: "Hotels" },
-    { title: "Hostels" },
-  ];
-
-  const cities = [
-    { title: "Tokyo" },
-    { title: "New Delhi" },
-    { title: "Beijing" },
-    { title: "Moscow" },
-    { title: "Seoul" },
-    { title: "Berlin" },
-    { title: "Jakarta" },
-    { title: "London" },
-    { title: "Brasília" },
-    { title: "Canberra" },
-    { title: "Ottawa" },
-    { title: "Washington D.C." },
-    { title: "Madrid" },
-    { title: "Paris" },
-    { title: "Rome" },
-    { title: "Amsterdam" },
-    { title: "Buenos Aires" },
-    { title: "Cairo" },
-    { title: "Dublin" },
-    { title: "Oslo" },
-    { title: "Lisbon" },
-    { title: "Stockholm" },
-    { title: "Athens" },
-    { title: "Vienna" },
-    { title: "Brussels" },
-    { title: "Helsinki" },
-    { title: "Prague" },
-    { title: "Warsaw" },
-    { title: "Budapest" },
-    { title: "Copenhagen" },
-    { title: "Reykjavik" },
-    { title: "Nicosia" },
-    { title: "Tallinn" },
-    { title: "Riga" },
-    { title: "Vilnius" },
-    { title: "Ljubljana" },
-    { title: "Zagreb" },
-    { title: "Sofia" },
-    { title: "Bucharest" },
-    { title: "Belgrade" },
-    { title: "Skopje" },
-    { title: "Podgorica" },
-    { title: "Tirana" },
-    { title: "Chisinau" },
-    { title: "Minsk" },
-    { title: "Kiev" },
-    { title: "Tbilisi" },
-    { title: "Yerevan" },
-    { title: "Baku" },
-  ];
-
-  const filter = createFilterOptions();
-  const [value, setValue] = useState(null);
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
 
   return (
-    <Box component='form' onSubmit={handleSubmit} 
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
       sx={{
         width: 500,
         height: 300,
         maxWidth: "md",
-        backgroundColor: "primary.white",
+        backgroundColor: "white",
         padding: 2,
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          gap: 2,
+      <h2>Search</h2>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="activities">Top 5</InputLabel>
+        <Select
+          labelId="activities"
+          id="activities"
+          name="activities"
+          multiple
+          value={activities}
+          onChange={handleActivityChange}
+          input={<OutlinedInput id="activities" label="activities" />}
+          renderValue={(selected) => (
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+              {selected.map((value) => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
+        >
+          {activitiesArray.map((activitity) => (
+            <MenuItem key={activitity} value={activitity}>
+              {activitity}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Box sx={{ m: 1, maxWidth: 300 }}>
+        <FormControl fullWidth>
+          <InputLabel id="city">City</InputLabel>
+          <Select
+            labelId="city"
+            id="city"
+            value={city}
+            label="city"
+            onChange={handleCityChange}
+          >
+            {citiesArray.map((city) => (
+              <MenuItem key={city} value={city}>
+                {city}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Button
+        type="submit"
+        variant="contained"
+        size="large"
+        style={{
+          minWidth: "50px",
+          // height: "100%",
+          margin: "20px",
+          padding: "10px",
+          alignSelf: "center",
         }}
       >
-        <h2>Search</h2>
-        <Autocomplete
-          multiple
-          id="checkboxes-tags-demo"
-          options={activities}
-          name='type'
-          disableCloseOnSelect
-          getOptionLabel={(option) => option.title}
-          size="small"
-          renderOption={(props, option, { selected }) => (
-            <li {...props}>
-              <Checkbox style={{ marginRight: 8 }} checked={selected} />
-              {option.title}
-            </li>
-          )}
-          style={{ width: 500 }}
-          renderInput={(params) => (
-            <TextField {...params} label="Top 5" placeholder="Activities" />
-          )}
-        />
-
-        <Autocomplete
-          value={value}
-          onChange={(event, newValue) => {
-            if (typeof newValue === "string") {
-              setValue({
-                title: newValue,
-              });
-            } else if (newValue && newValue.inputValue) {
-              // Create a new value from the user input
-              setValue({
-                title: newValue.inputValue,
-              });
-            } else {
-              setValue(newValue);
-            }
-          }}
-          filterOptions={(options, params) => {
-            const filtered = filter(options, params);
-
-            const { inputValue } = params;
-            // Suggest the creation of a new value
-            const isExisting = options.some(
-              (option) => inputValue === option.title
-            );
-            if (inputValue !== "" && !isExisting) {
-              filtered.push({
-                inputValue,
-                title: `Add "${inputValue}"`,
-              });
-            }
-
-            return filtered;
-          }}
-          selectOnFocus
-          clearOnBlur
-          handleHomeEndKeys
-          id="City"
-          name='location'
-          options={cities}
-          size="small"
-          getOptionLabel={(option) => {
-            // Value selected with enter, right from the input
-            if (typeof option === "string") {
-              return option;
-            }
-            // Add "xxx" option created dynamically
-            if (option.inputValue) {
-              return option.inputValue;
-            }
-            // Regular option
-            return option.title;
-          }}
-          renderOption={(props, option) => <li {...props}>{option.title}</li>}
-          sx={{ width: 300 }}
-          freeSolo
-          renderInput={(params) => <TextField {...params} label="In" />}
-        />
-
-        <Button
-          type='submit'
-          variant="contained"
-          size="large"
-          style={{
-            minWidth: "50px",
-            height: "100%",
-            margin: "20px",
-            padding: "10px",
-            alignSelf: "center",
-          }}
-        >
-          Search
-        </Button>
-      </Box>
+        Search
+      </Button>
     </Box>
   );
 }
