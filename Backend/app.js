a91b0cde41899b23f6703c23a73b5b2ed9daa26f
Meital Mishni <meital.mishni@gmail.com>
2023-03-28 19:33:29 +0300

    add free search route

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const cors = require("cors");
 require('dotenv').config();
-// const { Configuration, OpenAIApi } = require('openai');
+
 const SearchController = require('./controllers/SearchController');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 
 app.post('/filtered-search', SearchController.FilteredSearch);
-
+app.post('/free-search', SearchController.FreeSearch);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
